78aab98eb0afb2ae1d817891149324fbdba04c34
rajnivas <prenivas@gmail.com>
2020-05-14 18:49:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express ();
 
 app.get("/",function(req,res){
-  res.send("<h1>Hello World</h>");
+  res.send("<h1>Hello World ALL</h>");
 })
 
 app.listen(3000, function() {
